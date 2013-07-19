5fe7d86d94328e592f4cca59881b3d3e11419a52
Paul M. Jones <pmjones88@gmail.com>
2013-07-18 22:06:27 -0500

    update with notes

--- static.sh
+++ static.sh
@@ -2,5 +2,5 @@ wget --mirror --page-requisites -E --convert-links http://solarphp.com
 cd solarphp.com
 mv project.1.html project/index.html
 find . -type f -exec sed -i '' 's#project.1.html#project/#g' {} \;
-# copy public/Helios/App/Controller/images/*
-# because wget doesn't deal with css
+# wget missed: public/Helios/App/Controller/images/*
+# wget missed: public/Proxima/App/Controller/cssfw/nav-horizontal.css
